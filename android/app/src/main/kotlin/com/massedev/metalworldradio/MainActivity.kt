dcde8b52557fa2c9db22012e67f87349f03e6b6f
Franco Rodrigo Miranda <167719225+MirandaFrancoCBA@users.noreply.github.com>
2026-09-21 23:55:52 -0300

    chore: prepare Android app for V1 release (#13)
    * chore: configure production Android identity and versioning
    
    * chore: tighten Android production manifest
    
    * docs: set app package description
    
    * chore: move Android activity to production namespace
    
    * chore: remove default Android namespace activity
    
    * ci: validate Android build

--- android/app/src/main/kotlin/com/massedev/metalworldradio/MainActivity.kt
+++ android/app/src/main/kotlin/com/massedev/metalworldradio/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.metal_world_radio
+package com.massedev.metalworldradio
 
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
