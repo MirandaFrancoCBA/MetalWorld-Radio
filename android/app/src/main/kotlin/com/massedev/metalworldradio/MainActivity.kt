c1607f5e586f73d11308d5fb51e4a88309fc9d27
Franco Rodrigo Miranda <167719225+MirandaFrancoCBA@users.noreply.github.com>
2026-09-21 23:18:02 -0300

    chore: move Android activity to production namespace

---
+++ android/app/src/main/kotlin/com/massedev/metalworldradio/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.massedev.metalworldradio
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import com.ryanheise.audioservice.AudioServicePlugin
+
+class MainActivity : FlutterFragmentActivity() {
+    override fun provideFlutterEngine(context: android.content.Context): FlutterEngine {
+        return AudioServicePlugin.getFlutterEngine(context)
+    }
+
+    override fun getCachedEngineId(): String {
+        AudioServicePlugin.getFlutterEngine(this)
+        return AudioServicePlugin.getFlutterEngineId()
+    }
+
+    override fun shouldDestroyEngineWithHost(): Boolean {
+        return false
+    }
+}
